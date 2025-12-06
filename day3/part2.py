23e2753fe2e532eb5e84198b445963e2110f514e
Naut de Vroome <nautdevroome@gmail.com>
2025-12-06 18:57:34 +0100

    cleanup

--- day3/part2.py
+++ day3/part2.py
@@ -3,9 +3,12 @@ inp = [list(map(int, row)) for row in open('inp.txt').read().split('\n')]
 LENGTH = 12
 
 def solve(bank, depth):  
+  if depth <= 0:
+    return ""
+
   record = -1
   idx = -1
-  takeFromBack = -LENGTH+depth+1
+  takeFromBack = -depth+1
   if takeFromBack >= 0:
     takeFromBack = 99999999999
   for i, joltage in enumerate(bank[:takeFromBack]):
@@ -13,22 +16,13 @@ def solve(bank, depth):
       record = joltage
       idx = i
 
-  # print(bank, depth)
-  # print(record, idx)
-  # print('---')
-
-  if depth >= LENGTH - 1:
-    return record
-
-  return int(f"{record}{solve(bank[idx+1:], depth+1)}")
+  return f"{record}{solve(bank[idx+1:], depth-1)}"
 
 # print(solve(inp[1], 0))
 
 c = 0
 for bank in inp:
-  s = solve(bank, 0)
+  s = int(solve(bank, 12))
   c+=s
 
 print(c)
-
-# 811111111111119
